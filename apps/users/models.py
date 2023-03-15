7b6eaaef47b5deaa55026780147c53200b01f82b
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-03-15 12:32:11 -0400

    models User

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(
+        max_length=255,
+        verbose_name="Имя пользователя",
+        blank = True, null = True,
+        unique=True
+    )
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Почта",
+        blank = True, null = True,
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        verbose_name="Телефонный номер",
+        blank = True, null = True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    age = models.PositiveIntegerField(
+        verbose_name="Возраст",
+        blank = True, null = True,
+    )
+    balance = models.CharField(
+        max_length=255,
+        verbose_name="Баланс",
+        blank = True, null = True,
+        default=0
+    )
+    wallet_address = models.CharField(
+        max_length=12,
+        verbose_name="Кошелек",
+        blank = True, null = True,
+        unique=True
+    )
+
