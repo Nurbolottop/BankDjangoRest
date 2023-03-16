e9714968209afa7a9d78b89ca22848be7875098f
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-03-16 02:51:49 -0400

    transfer urls settings

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 #my imports
-from apps.users.views import UserAPIViewSet,CreateTransferView
+from apps.users.views import UserAPIViewSet
 
 router = DefaultRouter()
 router.register('users', UserAPIViewSet, basename="users")
@@ -14,7 +14,6 @@ router.register('users', UserAPIViewSet, basename="users")
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name = "api_login"),
     path('refresh/', TokenRefreshView.as_view(), name = "api_refresh"),
-    path('transfer/', CreateTransferView.as_view(), name = "transfer"),
     
 ]
 
